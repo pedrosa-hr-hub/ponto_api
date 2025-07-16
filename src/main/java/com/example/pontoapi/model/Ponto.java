9ed9d0c2e8b3aed5a358ae9707b484b6b2f1432a
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-07-15 23:05:26 -0300

    contruindo a entidade (tabela no banco)

---
+++ src/main/java/com/example/pontoapi/model/Ponto.java
@@ -0,0 +1,27 @@
+package com.example.pontoapi.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ponto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime horario;
+
+}
