0b352b86bc2b125b574552de0ed74bfe8948895b
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-07-17 20:48:52 -0300

    criando o repositório das chamadas no banco.

---
+++ src/main/java/com/example/pontoapi/repository/PontoRepository.java
@@ -0,0 +1,9 @@
+package com.example.pontoapi.repository;
+
+import com.example.pontoapi.model.Ponto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PontoRepository extends JpaRepository<Ponto, Long> {
+
+}
