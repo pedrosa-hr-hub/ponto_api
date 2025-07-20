21583af6883e73f0ee21f08fce3e77c7f62fd836
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-07-20 15:33:21 -0300

    implementação do controller, que recebe e direciona a requisição.

--- src/main/java/com/example/pontoapi/controller/PontoController.java
+++ src/main/java/com/example/pontoapi/controller/PontoController.java
@@ -1,5 +1,37 @@
 package com.example.pontoapi.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.pontoapi.dto.PontoResponse;
+import com.example.pontoapi.service.PontoService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/ponto")
 public class PontoController {
+
+    @Autowired
+    private PontoService pontoService;
+
+    @PostMapping
+    public ResponseEntity<PontoResponse> registrarPonto(@RequestBody PontoResponse dto) {
+
+        PontoResponse salvo = pontoService.registrar(dto);
+
+        return ResponseEntity.ok(salvo);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PontoResponse>> listarTdos() {
+        List<PontoResponse> ponto = pontoService.listarTodos();
+        return ResponseEntity.ok(ponto);
+    }
 
 }
