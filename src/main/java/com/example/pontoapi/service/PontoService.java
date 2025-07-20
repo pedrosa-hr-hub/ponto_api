2c082695f0c8adc41d366dfebf17be0607444c27
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-07-20 15:22:14 -0300

    implementação da camada service, logica do negocio.

--- src/main/java/com/example/pontoapi/service/PontoService.java
+++ src/main/java/com/example/pontoapi/service/PontoService.java
@@ -1,5 +1,31 @@
 package com.example.pontoapi.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.pontoapi.dto.PontoResponse;
+import com.example.pontoapi.model.Ponto;
+import com.example.pontoapi.repository.PontoRepository;
+
 public class PontoService {
 
+    @Autowired
+    private PontoRepository pontoRepository;
+
+    public PontoResponse registrar(PontoResponse dto) {
+
+        Ponto ponto = new Ponto(null, dto.getHorario());
+
+        Ponto salvo = pontoRepository.save(ponto);
+
+        return new PontoResponse(salvo.getId(), salvo.getHorario());
+    }
+
+    public List<PontoResponse> listarTodos() {
+        return pontoRepository.findAll().stream()
+                .map(p -> new PontoResponse(p.getId(), p.getHorario()))
+                .collect(Collectors.toList());
+    }
 }
