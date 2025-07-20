2b1498e1804298f6c5ce60f56435e5d9a1367657
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-07-20 16:59:21 -0300

    adicionando novos campos no banco de dados.

--- src/main/java/com/example/pontoapi/model/Ponto.java
+++ src/main/java/com/example/pontoapi/model/Ponto.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,6 +21,18 @@ public class Ponto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Getter
     private Long id;
+
+    @SequenceGenerator(name = "nsr_seq_generator", sequenceName = "nsr_sequence", allocationSize = 1)
+    @Getter
+    private Long NSR;
+
+    @Setter
+    @Getter
+    private String CPF;
+
+    @Setter
+    @Getter
+    private String PIS;
 
     @Setter
     @Getter
