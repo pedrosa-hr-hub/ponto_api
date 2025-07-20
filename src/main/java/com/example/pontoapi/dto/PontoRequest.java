d2881d1832867404185531b9f63a7f2a02b64bdc
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-07-20 19:12:17 -0300

    utilização de record para limpar o codigo e para boas praticas.

--- src/main/java/com/example/pontoapi/dto/PontoRequest.java
+++ src/main/java/com/example/pontoapi/dto/PontoRequest.java
@@ -1,11 +1,6 @@
 package com.example.pontoapi.dto;
 
 import java.time.LocalDateTime;
-import jakarta.validation.constraints.NotNull;
-import lombok.Data;
 
-@Data
-public class PontoRequest {
-    @NotNull
-    private LocalDateTime horario;
+public record PontoRequest(LocalDateTime horario) {
 }
--- src/main/java/com/example/pontoapi/dto/PontoResponse.java
+++ src/main/java/com/example/pontoapi/dto/PontoResponse.java
@@ -2,16 +2,5 @@ package com.example.pontoapi.dto;
 
 import java.time.LocalDateTime;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class PontoResponse {
-    private Long NSR;
-    private String CPF;
-    private String PIS;
-    private LocalDateTime horario;
+public record PontoResponse(Long NSR, String CPF, String PIS, LocalDateTime horario) {
 }
