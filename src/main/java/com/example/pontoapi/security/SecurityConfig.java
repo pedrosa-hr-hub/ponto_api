54d96e627ec8cebbd440d8108254970d9f878eb5
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-07-30 22:51:25 -0300

    implementação do filtro de segurança no spring, travando todas as rotas. precisa de autenticação em todas.

--- src/main/java/com/example/pontoapi/security/SecurityConfig.java
+++ src/main/java/com/example/pontoapi/security/SecurityConfig.java
@@ -1,5 +1,27 @@
 package com.example.pontoapi.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private JwtAuthFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated())
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    };
 }
