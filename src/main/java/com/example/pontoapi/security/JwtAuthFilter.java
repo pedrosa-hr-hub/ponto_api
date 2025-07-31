1da73ef8ef0c808e4f1e2488386a93a47279e651
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-07-30 22:29:41 -0300

    filtrando todas as requisições que chegam no serviço e vendo se elas tem o token e se são validos.

--- src/main/java/com/example/pontoapi/security/JwtAuthFilter.java
+++ src/main/java/com/example/pontoapi/security/JwtAuthFilter.java
@@ -1,5 +1,42 @@
 package com.example.pontoapi.security;
 
-public class JwtAuthFilter {
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer: ")) {
+
+            String token = authHeader.substring(7);
+
+            if (!jwtUtil.isTokenExpirado(token)) {
+
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+                return;
+            }
+
+        }
+
+        filterChain.doFilter(request, response);
+
+    };
 
 }
