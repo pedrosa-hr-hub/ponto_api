9657ef8eb08b4399f53d51cf28dcb684d508b0da
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-07-30 21:01:29 -0300

    ajuste no escopo da utilização do JWT

--- src/main/java/com/example/pontoapi/security/JwtUtil.java
+++ src/main/java/com/example/pontoapi/security/JwtUtil.java
@@ -2,26 +2,14 @@ package com.example.pontoapi.security;
 
 import java.util.Date;
 
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
 public class JwtUtil {
 
     private final String SECRET_KEY = "${SECRET_KEY_JWT}";
-
-    // Gera o token JWT
-    public String gerarToken(String username) {
-        return Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
-                .compact();
-    }
 
     // Extrai o usuario do token
     public String extraiUsuario(String token) {
@@ -34,18 +22,22 @@ public class JwtUtil {
 
     // Vê se o token está expirado
     private boolean isTokenExpirado(String token) {
-        final Date expiration = Jwts.parser()
-                .setSigningKey(SECRET_KEY)
-                .parseClaimsJws(token)
-                .getBody()
-                .getExpiration();
-        return expiration.before(new Date());
-    }
+        try {
 
-    // Vendo se o token é valido
-    public boolean validarToken(String token, UserDetails userDetails) {
-        final String username = extraiUsuario(token);
-        return username.equals(userDetails.getUsername()) && !isTokenExpirado(token);
+            final Date expiration = Jwts.parser()
+                    .setSigningKey(SECRET_KEY)
+                    .parseClaimsJws(token)
+                    .getBody()
+                    .getExpiration();
+
+            return expiration.after(new Date());
+
+        } catch (Exception e) {
+
+            return false;
+
+        }
+
     }
 
 }
