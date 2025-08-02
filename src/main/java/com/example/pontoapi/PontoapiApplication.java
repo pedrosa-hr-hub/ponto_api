ec68be182002fa4c7dec367e8b20396903b9f5b7
Pedro de Sá <pedro.trabalhos@outlook.com>
2025-08-02 16:41:26 -0300

    Carregando o dotenv em toda a aplicação.

--- src/main/java/com/example/pontoapi/PontoapiApplication.java
+++ src/main/java/com/example/pontoapi/PontoapiApplication.java
@@ -3,10 +3,13 @@ package com.example.pontoapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class PontoapiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
 		SpringApplication.run(PontoapiApplication.class, args);
 
 		System.err.println("Está rodando!");
